f0d073f307b65d360d41e31e0fae4338fa942a00
NeerajaAnil005 <neerajaanil22@gmail.com>
2025-07-18 22:29:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('cta-btn').addEventListener('click', function() {
+    alert("You've just ordered a bottle of LushLocks Elixir! (Not really, but you wish you did!)");
+});
+function fakePurchase() {
+    alert("🎉 Thank you for your interest!\nYour magical bottle of LushLocks Elixir is on its (imaginary) way! ✨");
+}
